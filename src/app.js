a6a45a82c11ea68945e30be72efeb5e3f48a337b
Pranav4op <139647747+Pranav4op@users.noreply.github.com>
2025-09-02 10:16:25 +0530

    add express and cors setup with initial server configuration

---
+++ src/app.js
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
+
+app.get("/pranav", (req, res) => {
+  res.send("This is still Pranav's page but from App");
+});
+
+export default app;
